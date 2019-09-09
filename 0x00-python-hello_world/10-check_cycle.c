0694184a6cf98284f796a191b435190f2469fc6e
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-09 22:34:29 +0000

    [BUGFIX] Fix efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,28 +8,14 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list, *fast = list;
 
-	while (list && (*list).next)
+	while (slow && fast && (*fast).next)
 	{
-		slow = list;
-		fast = (*list).next;
-		while (slow != fast)
-		{
-			if (slow)
-				slow = (*slow).next;
-			if (fast == list)
-				return (1);
-			if (fast)
-				fast = (*fast).next;
-			if (fast == list)
-				return (1);
-			if (fast)
-				fast = (*fast).next;
-			if (fast == list)
-				return (1);
-		}
-		list = (*list).next;
+		slow = (*slow).next;
+		fast = (*(*fast).next).next;
+		if (slow == fast)
+			return (1);
 	}
 	return (0);
 }
