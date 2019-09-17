3384a05ae0de49e42196de7349da6a5e1f3b5e5f
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-17 13:56:11 -0400

    [TASK] Print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    for row in range(rows):
+        values = len(row)
+        for val in range(values):
+            if (val != values - 1):
+                print("{:d}".format(matrix[row][val]), end=" ")
+            else:
+                print("{:d}".format(matrix[row][val]))
