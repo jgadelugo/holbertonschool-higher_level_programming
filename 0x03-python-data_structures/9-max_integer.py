66a7977a95fa6e82f4a42584dcab2a6c4ab65032
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-09-17 22:35:43 -0400

    [TASK] Find Max in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
     max = my_list[0]
     for i in my_list:
