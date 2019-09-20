88b7387838745f0962f576e743edc7c2c5b898d9
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-09-20 13:43:31 -0400

    [BUGFIX] Check if varibale is a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or len(roman_string) == 0 or type(roman_string) != str:
+    if roman_string is None or len(roman_string) == 0 or not (isinstance(roman_string, str)):
         return 0
     roman_nums = {"I" : 1,
                 "V" : 5,
