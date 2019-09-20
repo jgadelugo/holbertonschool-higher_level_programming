042a470f63cba3bf796b5e635a7d48920b748f3b
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-20 19:48:21 +0000

    [TASK] Fix indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_nums = {"I" : 1,
-                "V" : 5,
-                "X" : 10,
-                "L" : 50,
-                "C" : 100,
-                "D" : 500,
-                "M" : 1000}
+    roman_nums = {"I": 1,
+                  "V": 5,
+                  "X": 10,
+                  "L": 50,
+                  "C": 100,
+                  "D": 500,
+                  "M": 1000}
 
     nextR = ""
     num = 0
