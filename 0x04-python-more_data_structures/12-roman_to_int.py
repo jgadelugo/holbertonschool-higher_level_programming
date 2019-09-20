ad486a213d06570222ab741f36707f8019b3e32f
Jose Alvarez de Lugo <48131843+jgadelugo@users.noreply.github.com>
2019-09-19 23:09:43 -0400

    [BUGFIX] check for empty string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return 0
     roman_nums = {"I" : 1,
                 "V" : 5,
                 "X" : 10,
