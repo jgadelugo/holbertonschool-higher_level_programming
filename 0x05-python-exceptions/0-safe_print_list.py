c64764faff2e6d8e3ed1e30427a188c9ab0772bf
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-30 16:35:29 +0000

    [BUGFIX] Continue through list even after exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
+        count += 1
         try:
             print("{}".format(my_list[i]), end="")
         except:
-            i -= 1
-            break
-    i += 1
+            count -= 1
     print()
-    return i
+    return count
