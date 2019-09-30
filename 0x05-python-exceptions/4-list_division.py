b85057a72faa998aae43436f7e642d2d4af00705
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-30 16:29:46 +0000

    [TASK] Divide each element of one list by each element of another list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    for i in range(list_length):
+        div_list.append(0)
+        try:
+            div_list[i] = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+    return div_list
+        
