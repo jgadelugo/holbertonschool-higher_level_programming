58804c2fa674936dd213a69307437a0c88e8be80
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-01 23:40:47 -0400

    [TASK] Add private instance attribute position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,9 +7,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-        return self.__size ** 2
-
     @property
     def size(self):
         return self.__size
@@ -22,15 +19,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        printSize = self.__size
-        if printSize == 0:
-            print()
-        for i in range(printSize):
-            for j in range(printSize):
-                print("#", end="")
-            print()
-    
     @property
     def position(self):
         return self.__position
@@ -47,3 +35,15 @@ class Square:
             raise errMsg
 
         self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        printSize = self.__size
+        if printSize == 0:
+            print()
+        for i in range(printSize):
+            for j in range(printSize):
+                print("#", end="")
+            print()
