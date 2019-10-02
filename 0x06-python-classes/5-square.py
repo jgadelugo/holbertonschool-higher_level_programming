92bc5465f56fed7239cfaac6f7c7dd207dd9fa09
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-01 23:17:32 -0400

    [TASK] Add print square method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        printSize = self.__size
+        if printSize == 0:
+            print()
+        for i in range(printSize):
+            for j in range(printSize):
+                print("#", end="")
+            print()
