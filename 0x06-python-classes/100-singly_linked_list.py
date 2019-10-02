96a53527b7f1a7353ad80fee3793746c567c75e3
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-01 23:54:59 -0400

    [TASk] Create file for task

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+class Node:
+    def data(self):
+        pass
+    def next_node(self):
+        pass
+    def next_node(self, value):
+        pass
+    def __init__(self):
+        pass
