45ba8c7cc453b5fa416ec48cb1257465d87365ac
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-15 20:44:37 +0000

    [TASK] Add function to check if obj is instance of class or inherits from class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of, or if the object
+is instance of a class that inherited from, the specified class
+"""
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object is an instance of or inst. of inh."""
+    return isinstance(obj, a_class)
+
