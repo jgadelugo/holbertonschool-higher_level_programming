34f7e4d35051a86cff8733957d89e1d2452dce60
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-15 20:51:52 +0000

    [TASK] Add public method area but do not implement it

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+class BaseGeometry:
+    """ class BaseGeometry"""
+    def area(self):
+        """ public instance method not implemented"""
+        raise Exception("area() is not implemented")
