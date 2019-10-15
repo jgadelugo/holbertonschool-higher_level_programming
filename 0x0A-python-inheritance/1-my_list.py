04fa8f20ff17444a287db5875213210c9562719a
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-15 19:03:27 +0000

    [TASK] Add a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+class MyList that inherits from list
+"""
+class MyList(list):
+    """class that inherits from list"""
+    def print_sorted(self):
+        """ print sorted list"""
+        print(sorted(self))
