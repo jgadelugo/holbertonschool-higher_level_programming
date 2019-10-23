3222db0f5186d2a397e52128b9f1b2bc88b23e5c
Jose Alvarez de Lugo <48131843+jgadelugo@users.noreply.github.com>
2019-10-23 11:04:43 -0400

    [TASK] Add pep8 test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,3 +18,10 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r3.id, 15)
         with self.assertRaises(TypeError):
             Rectangle(10, 2, 3, 4, 5, 5)
+
+     def test_pep8(self):
+        """ tests prp8 formating """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style erros (and warnings).")
