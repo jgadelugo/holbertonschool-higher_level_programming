96d0f94e31d3dd1656b7a98331a78158d2ee819c
Jose Alvarez de Lugo <48131843+jgadelugo@users.noreply.github.com>
2019-10-23 11:05:57 -0400

    [TASK] Add pep8 check

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -11,3 +11,10 @@ class TestSquare(unittest.TestCase):
     def test_area(self):
         s1 = Square(5)
         self.assertEqual(s1.area(), 25)
+
+    def test_pep8(self):
+        """ tests prp8 formating """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style erros (and warnings).")
