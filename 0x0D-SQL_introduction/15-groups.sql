0b5953bf5dc4d4d0db510ae2fafacfb12eee75e4
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-11-26 03:56:42 +0000

    [BUGFIX] Style

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
 -- get count of scores
-SELECT score, COUNT(score) AS number
-FROM second_table
-GROUP BY score ORDER BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,2 @@
 --script that lists all records with names
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
