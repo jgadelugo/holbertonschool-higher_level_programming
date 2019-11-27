4fda650038eb7cb543b0d2ede78c3a0219466109
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-11-27 02:19:14 +0000

    [TASK] Create database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Script that creates the table unique_id on your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
