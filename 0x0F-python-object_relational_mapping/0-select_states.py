fc6cb2b2f92c126d5ac2ba2c51327026c23dc1a6
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-12-13 21:38:33 +0000

    [PROJECT] MySQLdb and SQLALCHEMY - Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# script to list all states from database hbtn_0e_0_usa
+from sys import argv as av
+import MySQLdb
+
+
+if __name__ == "__main__":
+    ac = len(av)
+    if ac < 4:
+        exit()
+    conn = MySQLdb.connect(host = "localhost",
+                           port = 3306,
+                           user = av[1],
+                           passwd = av[2],
+                           db = av[3],
+                           charset = "utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
