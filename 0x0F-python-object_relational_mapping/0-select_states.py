2d91e670d2b9f55a70bfcc74572ebd885a9a80fb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-12-16 13:30:12 -0500

    [BUGFIX] Use linter

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     ac = len(av)
     if ac < 4:
         exit()
-    conn = MySQLdb.connect(host = "localhost",
-                           port = 3306,
-                           user = av[1],
-                           passwd = av[2],
-                           db = av[3],
-                           charset = "utf8")
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=av[1],
+                           passwd=av[2],
+                           db=av[3],
+                           charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
