6ad45891a831fe6c661601fb023ed257abe7d1bf
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-12-15 22:33:40 -0500

    [TASK] Search database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                            db=av[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1][0] == "N":
