d912506c7aa65549bae09c144ee6a0b04b0741a6
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-12-15 21:51:51 -0500

    [TASK] query a database using python MySQLdb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,9 +16,10 @@ if __name__ == "__main__":
                            db=av[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
     cur.close()
     conn.close()
