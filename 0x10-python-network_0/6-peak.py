c82b99fed7d68312bb6d813324029f1c848545e3
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2020-01-06 18:54:41 -0500

    [TASK] Find a peak in an unsorted list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,7 @@ def find_peak(list_of_integers):
     if list_of_integers is None or list_of_integers is []:
         return None
     size = len(list_of_integers)
-    if size == 0:
+    if size == 1:
         return list_of_integers[0]
     for i in range(0, size - 1):
         if list_of_integers[i] > list_of_integers[i + 1]:
