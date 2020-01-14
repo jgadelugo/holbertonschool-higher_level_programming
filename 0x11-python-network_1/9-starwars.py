4be066a63795fd99d34cf5de4ded866376426f9b
Jose Alvarez de Lugo <845@holbertonschool.com>
2020-01-13 23:18:56 -0500

    [TASK] Grab data from starwars api

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" get data from starwars api """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/"
+    data = {"search": argv[1]}
+    res = requests.get(url, data)
+    _json = res.json()
+    print("Number of results:", _json.get("count"))
+    for name in _json.get("results"):
+        print(name.get("name"))
