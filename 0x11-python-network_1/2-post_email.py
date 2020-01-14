7830f33b4c46b6b0d88d28a3bc677d33389b00fd
Jose Alvarez de Lugo <845@holbertonschool.com>
2020-01-13 21:59:47 -0500

    [TASK] Send post request using urllib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Sends a POST request with email and return body """
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    email = {"email": argv[2]}
+    data = parse.urlencode(email)
+    with request.urlopen(argv[1], data.encode()) as f:
+        body = f.read().decode('utf-8')
+        print(body)
