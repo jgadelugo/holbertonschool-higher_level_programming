ac7cf80b80d4c09f887e3678497eedc6827457c5
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2020-01-13 20:15:04 -0500

    [TASK] Use urllib to fetch a websites content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
         print('Body response:')
-        print('\t- type: ' +type(body))
-        print('\t- content: ' + body)
-        print('\t- utf8 content: ' + body.decode('UTF-8'))
+        print('\t- type:', type(body))
+        print('\t- content:', body)
+        print('\t- utf8 content:', body.decode('UTF-8'))
