cbb086256a27b26f27bd7454e81460167023f833
Jose Alvarez de Lugo <845@holbertonschool.com>
2020-01-13 22:37:05 -0500

    [TASK] Use requests to get response and print

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status").text
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(response))
-    print("\t- type:", response)
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)
