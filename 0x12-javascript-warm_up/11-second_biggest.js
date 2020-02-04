5a77e194c80502b058116c99acd5013fc7512e40
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-04 15:21:26 -0500

    [TASK] Return second highest int in an array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,13 +4,8 @@ const a = process.argv;
 const size = a.length;
 
 function secondBiggest (a) {
-  const unique = [...new Set(a)];
-  if (unique.length < 2) {
-    return unique[0];
-  }
-  unique.sort();
-  unique.reverse();
-  return unique[1];
+  a.sort((a, b) => b - a);
+  return a[1];
 }
 
 if (size < 4) {
