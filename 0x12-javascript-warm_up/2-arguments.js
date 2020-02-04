7cca24e1cf3caae01dc374d5ff8fe12a409c6227
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-04 12:38:34 -0500

    [TASK] Use if statement and argv in javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const first = 'No argument';
+const second = 'Argument found';
+const third = 'Arguments found';
+
+var numOfArgs = process.argv.length;
+
+if (numOfArgs < 3) {
+  console.log(first);
+} else if (numOfArgs < 4) {
+  console.log(second);
+} else {
+  console.log(third);
+}
