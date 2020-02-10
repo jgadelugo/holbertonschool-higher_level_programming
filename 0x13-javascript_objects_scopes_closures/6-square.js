259f0f24187a7241484156208bd55beb1f721ef3
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-10 15:50:27 -0500

    [TASK] Add charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const square = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends square {
   constructor (size) {
     super(size, size);
   }
