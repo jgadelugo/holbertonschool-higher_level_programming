33f0870da608238046c92e3628d07aef0edef6e3
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-11 23:31:30 -0500

    [TASK] Read file print err if fails

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const argv = process.argv;
 
 fs.readFile(argv[2], 'utf8', (err, file) => {
-if (err) {
-  console.log(err);
-} else {
-  console.log(file);
-}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(file);
+  }
 });
