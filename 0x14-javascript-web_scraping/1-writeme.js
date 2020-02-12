bb127e1d01315a3fbc889a1d7f7a2d98d565c5fd
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-11 23:37:29 -0500

    [TASK] Write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const argv = process.argv;
 
-const file = argv[2]
-const text = argv[3]
+const file = argv[2];
+const text = argv[3];
 
 fs.writeFile(file, text, (err) => {
   if (err) {
