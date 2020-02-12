7119b1b76dc98d936b56c0c2fa6129c3e19c05fe
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-02-12 00:57:23 -0500

    [TASK] bugfix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,30 +1,27 @@
 #!/usr/bin/node
 
-const request = require("request")
+const request = require('request');
 const argv = process.argv;
-const url = argv[2]
+const url = argv[2];
 
-
-function getStatus(theUrl)
-{
-    const options = {
-        url: theUrl,
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Accept-Charset': 'utf-8'
-        }
-    };
-    request(options, function(err, res, body) {
-        if (err) {
-            console.log(err)
-        } else {
-            //let json = JSON.parse(body);
-            let status = res.statusCode
-            console.log("code: " + status);
-            return status;
-        }
-    });
+function getStatus (theUrl) {
+  const options = {
+    url: theUrl,
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Accept-Charset': 'utf-8'
+    }
+  };
+  request(options, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      // let json = JSON.parse(body);
+      const status = res.statusCode;
+      console.log('code: ' + status);
+    }
+  });
 }
 
-getStatus(url)
+getStatus(url);
